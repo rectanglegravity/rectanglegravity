197bd4084c58d4f1ebb54c81089c986936a71648
rectanglegravity <rectanglegravity.com>
2023-03-10 18:34:06 +0300

    <commit message>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import './App.css'
 
@@ -8,11 +10,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <Box 
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
       <Typography color="secondary"  variant="overline" gutterBottom>
         rectangle gravity :)
       </Typography>
-    </div>
+      <CircularProgress color="success" />
+    </Box>
   )
 }
 
